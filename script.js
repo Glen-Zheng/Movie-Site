057ae16b206d0c77f1746e08280bddbd8af5e0c5
Glen Zheng <zhengglen200@gmail.com>
2022-11-01 22:13:40 -0400

    Styled some of the dynamic css and made a few important modifications

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let budget;
 let runtime;
 let tagline;
 let homepage;
-let popularity;
+let ranking;
 let collection;
 let collectionPoster;
 let trailer;
@@ -39,6 +39,7 @@ async function movieOutput(i) {
   trailers = video.data.videos.results.filter(
     (trailer) => trailer.type === "Trailer"
   );
+
   movieGenre = video.data.genres[0].name;
   movieBudget = video.data.budget;
   movieRevenue = video.data.revenue;
@@ -47,31 +48,45 @@ async function movieOutput(i) {
   movieTagline = video.data.tagline;
 
   title = document.createElement("h1");
+title.setAttribute("id", "movie-title");
   poster = document.createElement("img");
+poster.setAttribute("id", "movie-poster");
   overview = document.createElement("p");
+overview.setAttribute("id", "movie-overview");
   releaseDate = document.createElement("p");
+releaseDate.setAttribute("id", "movie-release-date");
   genre = document.createElement("p");
+genre.setAttribute("id", "movie-genre");
   revenue = document.createElement("p");
+revenue.setAttribute("id", "movie-revenue");
   budget = document.createElement("p");
+budget.setAttribute("id", "movie-budget");
   runtime = document.createElement("p");
+runtime.setAttribute("id", "movie-runtime");
   tagline = document.createElement("h2");
+tagline.setAttribute("id", "movie-tagline");
   homepage = document.createElement("a");
-  popularity = document.createElement("p");
+homepage.setAttribute("id", "movie-homepage");
+homepage.setAttribute("target", "_blank");
+  ranking = document.createElement("p");
+ranking.setAttribute("id", "movie-ranking");
   trailer = document.createElement("iframe");
+trailer.setAttribute("id", "movie-trailer");
 
   title.innerHTML = `${i.title} `;
   poster.src = `https://image.tmdb.org/t/p/w500${i.poster_path}`;
-  overview.innerhtml = `${i.overview}`;
-  releaseDate.innerHTML = `${i.release_date}`;
-  genre.innerHTML = `${movieGenre}`;
-  revenue.innerHTML = `${movieRevenue}`;
-  budget.innerHTML = `${movieBudget}`;
-  runtime.innerHTML = `${movieRuntime}`;
+  overview.innerHTML = `Overview: ${i.overview}`;
+  releaseDate.innerHTML = `Release Date: ${i.release_date}`;
+  genre.innerHTML = `Genre: ${movieGenre}`;
+  revenue.innerHTML = `Revenue: $${movieRevenue}`;
+  budget.innerHTML = `Budget: $${movieBudget}`;
+  runtime.innerHTML = `Movie Runtime: ${movieRuntime} minutes`;
   tagline.innerHTML = `${movieTagline}`;
   homepage.href = `${movieHomepage}`;
-  popularity.innerHTML = `${i.popularity}`;
+  homepage.innerHTML = "Movie Homepage";
+  ranking.innerHTML = `Ranking: ${i.vote_average}/10`;
 
-  trailer.src = `https://www.youtube.com/embed/${trailers[0]}`;
+  trailer.src = `https://www.youtube.com/embed/${trailers[0].key}`;
 
   div.append(title);
   div.append(poster);
@@ -83,13 +98,15 @@ async function movieOutput(i) {
   div.append(runtime);
   div.append(tagline);
   div.append(homepage);
-  div.append(popularity);
+  div.append(ranking);
   if (video.data.belongs_to_collection) {
     movieCollection = video.data.belongs_to_collection.name;
     movieCollectionPoster = video.data.belongs_to_collection.poster_path;
 
     collection = document.createElement("p");
+  collection.setAttribute("id", "movie-collection");
     collectionPoster = document.createElement("img");
+  collectionPoster.setAttribute("id", "movie-collection-poster");
     collection.innerHTML = `${movieCollection}`;
     collectionPoster.src = `https://image.tmdb.org/t/p/w500${movieCollectionPoster}`;
     div.append(collection);
@@ -126,7 +143,7 @@ get.addEventListener("click", async () => {
       case 424139:
         movieOutput(i);
         break;
-      case 1359:
+      case 354912:
         movieOutput(i);
 
         break;
@@ -158,7 +175,7 @@ get.addEventListener("click", async () => {
         movieOutput(i);
 
         break;
-      case 205596:
+      case 396371:
         movieOutput(i);
 
         break;
